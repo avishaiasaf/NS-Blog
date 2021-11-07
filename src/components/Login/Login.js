72acae46566cd5da1ddaef33f6a8ec5c037822f6
Avishai Asaf <avishaiasaf@Avishais-MacBook-Pro.local>
2021-11-07 09:12:26 +0200

    changing localhost

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -18,7 +18,7 @@ class Login extends Component{ //({ onLoginClick })=>{
     }
 
     onSubmit = ()=>{
-        fetch('http://localhost:3000/signin', {
+        fetch('https://pure-beyond-10290.herokuapp.com/signin', {
             'method': 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
