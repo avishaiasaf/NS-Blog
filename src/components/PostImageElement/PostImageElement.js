c75949af9b37d5895c30a8d2c651d30f68370a24
Avishai Asaf <avishaiasaf@Avishais-MacBook-Pro.local>
2021-12-30 11:09:37 +0200

    Image Fixing

--- src/components/PostImageElement/PostImageElement.js
+++ src/components/PostImageElement/PostImageElement.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PostImageElement = ({ imgSrc })=>{
     return (
         <div>
-            <img src={imgSrc} className='tc' alt="" width="70%" />
+            <img src={imgSrc} className='tc' alt="" width="70%" style={{border: '5px solid black;'}} />
         </div>
     );
 }
